90e9050c4504ee58ef0fe4eec32088009c395c90
vikornienko <vikornienko76@gmail.com>
2022-12-12 21:18:58 +0500

    Second commit Adding files in appmain.

---
+++ appmain/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth import views
+
+from .views import index, signup, myaccount, edit_myaccount, shop
+from product.views import product
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login', views.LogoutView.as_view(template_name='appmain/login.html'), name='login'),
+    path('myaccount/', myaccount, name='myaccount'),
+    path('edit_myaccount/', edit_myaccount, name='edit_myaccount'),
+    path('shop/', shop, name='shop'),
+    path('shop/<slug:slug>/', product, name='product'),
+]
--- appmain/views.py
+++ appmain/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-# Create your views here.
+from product.models import Product, Category
+
+from .forms import SignUpForm
+def index(request):
+    return render(request, 'appmain/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'appmain/signup.html', {'form': form})
+
+@login_required
+def myaccount(request):
+    return render(request, 'appmain/myaccount.html')
+
+@login_required
+def edit_myaccount(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.email = request.POST.get('email')
+        user.username = request.POST.get('username')
+        user.save()
+
+        return redirect('myaccount')
+    return render(request, 'appmain/edit_myaccount.html')
+
+def shop(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    active_category = request.GET.get('category', '')
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context_page = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
